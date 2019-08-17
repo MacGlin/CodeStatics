548d8b651f3beb4c897d4a5e3cdf0963c004be0b
XD.Wang <xiandui.wxd@alibabapictures.com>
2019-08-17 14:02:46 +0800

    init with spring boot

---
+++ src/main/java/com/macglin/codeStatistics/CodeStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.macglin.codeStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeStatisticsApplication.class, args);
+	}
+
+}
