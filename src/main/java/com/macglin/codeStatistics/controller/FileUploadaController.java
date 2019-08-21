705d12dbd64e0471fda67f9f7b46617fc5e1ab36
MacGlin <584973689@qq.com>
2019-08-21 19:06:26 +0800

    静态页面测试

--- src/main/java/com/macglin/codeStatistics/controller/FileUploadaController.java
+++ src/main/java/com/macglin/codeStatistics/controller/FileUploadaController.java
@@ -6,16 +6,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.lang.StringUtils;
 
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 
 @Controller
 public class FileUploadaController {
 
-    @RequestMapping("index")
+    //代码总行数
+    int totalNum;
+    //注释行数
+    int commentNum;
+    //空行
+    int emptyNum;
+    //实际代码行数
+    int codeNum;
+
+    @RequestMapping("/")
     public String index(){
         return "index";
     }
@@ -28,12 +40,56 @@ public class FileUploadaController {
         }
         try {
             byte[] bytes = file.getBytes();
-            Path path = Paths.get("E:\\fileUpload/" + file.getOriginalFilename());
+            Path path = Paths.get("D:\\fileUpload/" + file.getOriginalFilename());
             Files.write(path,bytes);
-            model.addAttribute("message","success");
+            File nFile = new File("D:\\fileUpload\\" + file.getOriginalFilename());
+            parse(nFile);
+            String message = "解析成功" +
+                    "文件名为" + file.getOriginalFilename() +
+                    "代码总行数为" + totalNum +
+                    "注释行数为" + commentNum +
+                    "空行为" + emptyNum +
+                    "实际代码行数为" + codeNum;
+            model.addAttribute("message",message);
         }catch (Exception e){
             e.printStackTrace();
         }
         return "/uploadStatus";
+    }
+
+    public String parse(File nFile){
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(nFile));
+            Stream<String> lines = bf.lines();
+            lines.forEach(s -> {
+                if (s.contains(";")){
+                    String[] lineItem = s.split(";");
+                    for (String item : lineItem) {
+                        lineParse(item);
+                    }
+                }
+                lineParse(s);
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 解析以"；"分割后的行
+     * @param ss
+     */
+    public void lineParse(String ss){
+        totalNum++;
+        ss = ss.trim();
+        if (ss.startsWith("//")||ss.startsWith("/*")||ss.startsWith("*")){
+            commentNum++;
+        }else if (StringUtils.isBlank(ss)){
+            emptyNum++;
+        }else {
+            codeNum++;
+        }
     }
 }
